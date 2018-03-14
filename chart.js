cbbd40f3af7c0926c07b541ba6b9934249f5aa94
George Margaritis <gmargaritis@users.noreply.github.com>
2018-03-14 18:56:24 +0200

    colour + search

--- chart.js
+++ chart.js
@@ -367,4 +367,64 @@ $(document).ready(function() {
 
 });
 
+function mouseover(d, i) {
+	// tooltip popup
+	var mosie = d3.select(this);
+	var amount = mosie.attr("amount");
+	var donor = d.donor;
+	var party = d.partyLabel;
+	var entity = d.entityLabel;
+	var offset = $("svg").offset();
+	
+
+
+	// image url that want to check
+	var imageFile = "https://raw.githubusercontent.com/ioniodi/D3js-uk-political-donations/master/photos/" + donor + ".ico";
+
+	
+	
+	// *******************************************
+	
+	
+	
+
+	
+
+	
+	var infoBox = "<p> Source: <b>" + donor + "</b> " +  "<span><img src='" + imageFile + "' height='42' width='42' onError='this.src=\"https://github.com/favicon.ico\";'></span></p>" 	
+	
+	 							+ "<p> Recipient: <b>" + party + "</b></p>"
+								+ "<p> Type of donor: <b>" + entity + "</b></p>"
+								+ "<p> Total value: <b>&#163;" + comma(amount) + "</b></p>";
+	
+	
+	mosie.classed("active", true);
+	d3.select(".tooltip")
+  	.style("left", (parseInt(d3.select(this).attr("cx") - 80) + offset.left) + "px")
+    .style("top", (parseInt(d3.select(this).attr("cy") - (d.radius+150)) + offset.top) + "px")
+		.html(infoBox)
+			.style("display","block");
+	
+	
+	}
+
+function mouseout() {
+	// no more tooltips
+		var mosie = d3.select(this);
+
+		mosie.classed("active", false);
+
+		d3.select(".tooltip")
+			.style("display", "none");
+		}
+
+$(document).ready(function() {
+		d3.selectAll(".switch").on("click", function(d) {
+      var id = d3.select(this).attr("id");
+      return transition(id);
+    });
+    return d3.csv("data/7500up.csv", display);
+
+});
+
 
